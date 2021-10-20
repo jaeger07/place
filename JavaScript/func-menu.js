6204d5be526440a499881f0f0fe13185dfb86e92
jaeger07 <jaeger1717@gmail.com>
2021-10-20 19:18:29 -0300

    ok

--- JavaScript/func-menu.js
+++ JavaScript/func-menu.js
@@ -42,7 +42,7 @@
      if(e.name == 'legislacao'){
         titulo.innerHTML = "Legislação";
         texto.innerHTML = "Parametrizamos os planos diretores para entender todas as condicionantes legais que impactam o terreno, analisando desde zona, altura máxima e coeficiente de aproveitamento até exceções como patrimônio histórico e áreas de proteção ambiental.";
-        destaque.innerHTML = "Cálculo de outorga onerosa <br>em tempo real.";
+        destaque.innerHTML = "Cálculo de outorga onerosa <br>em tempo real";
         gif.style.background = "url(../imagens/05_funcionalidade/Gifs/legislacao.gif) no-repeat cover";
         gif.style.backgroundSize = 'cover';
      }
@@ -78,7 +78,7 @@
      if(e.name == 'dashboard'){
         titulo.innerHTML = "Dashboard de terrenos"
         texto.innerHTML = "Salve seus estudos e visualize no painel de estudos de viabilidade, tendo acesso a histórico, comentários e possibilidade de rankeamento por indicadores."
-        destaque.innerHTML = "Sem limite de <br>estudos salvos."
+        destaque.innerHTML = "Sem limite de <br>estudos salvos"
         gif.style.background = "url(../imagens/05_funcionalidade/Gifs/dashboard.gif) no-repeat"
         gif.style.backgroundSize = 'cover'
      }
@@ -90,7 +90,7 @@
      if(e.name == 'camadas'){
         titulo.innerHTML = "Camadas"
         texto.innerHTML = "Visualize no mapa e interaja com camadas de análise urbanística, potencial construtivo, lançamentos imobiliários, insights de incorporação e muito mais!"
-        destaque.innerHTML = "+ de 7 milhões de dados <br>organizados e avaliados."
+        destaque.innerHTML = "+ de 7 milhões de dados <br>organizados e avaliados"
         gif.style.background = "url(../imagens/05_funcionalidade/Gifs/camadas.gif) no-repeat"
         gif.style.backgroundSize = 'cover'
      }
@@ -102,7 +102,7 @@
      if(e.name == 'filtros'){
         titulo.innerHTML = "Filtros"
         texto.innerHTML = "Combine e personalize camadas de dados para visualizar apenas os lotes que se adequam ao seu perfil de incorporação."
-        destaque.innerHTML = "+ de 7 milhões de dados <br>organizados e avaliados."
+        destaque.innerHTML = "+ de 7 milhões de dados <br>organizados e avaliados"
         gif.style.background = "url(../imagens/05_funcionalidade/Gifs/filtros.gif) no-repeat"
         gif.style.backgroundSize = 'cover'
      }
