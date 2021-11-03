24e023401b78ef5b81741611c985a04b9f879031
jaeger07 <jaeger1717@gmail.com>
2021-11-03 17:17:10 -0300

    ok

--- JavaScript/scroll-menu.js
+++ JavaScript/scroll-menu.js
@@ -1,16 +1,22 @@
+var atualPosicao;
+var ultimaPosicao;
 
 window.addEventListener("scroll",function() {
   const nav = document.querySelector("nav");
+  const div1 = document.querySelector("#div1");
+
+  atualPosicao = window.scrollY;
   
-  console.log(window.scrollY);
-  
-  if(window.scrollY > 0){
+  if(atualPosicao < (ultimaPosicao +6)){
+    div1.classList.add("div1-margin");
     nav.classList.add("sticky");
   }
-  else{
-    nav.classList.remove("sticky")
+  if((atualPosicao > ultimaPosicao)|| atualPosicao < 550 ){
+    div1.classList.remove("div1-margin");
+    nav.classList.remove("sticky");
   }
-
+  
+  ultimaPosicao = window.scrollY;
 })
 
 
