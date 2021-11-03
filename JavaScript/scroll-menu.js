49635b3da9116f9e088c57170dba53cff179f99a
jaeger07 <jaeger1717@gmail.com>
2021-11-03 15:40:14 -0300

    ok

--- JavaScript/scroll-menu.js
+++ JavaScript/scroll-menu.js
@@ -1,33 +1,16 @@
 
-const debounce = function(func, wait, immediate) {
-  let timeout;
-  return function(...args) {
-    const context = this;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
-
-
-const menu = document.querySelector('.nav');
-const pap = document.querySelector('#PAP')
-
-function menuScroll() {
-  if(window.pageYOffset > pap.offsetTop) {
-    menu.classList.add('menu-scroll');
-  } else {
-    menu.classList.remove('menu-scroll');
+window.addEventListener("scroll",function() {
+  const nav = document.querySelector("nav");
+  
+  console.log(window.scrollY);
+  
+  if(window.scrollY > 0){
+    nav.classList.add("sticky");
   }
-}
+  else{
+    nav.classList.remove("sticky")
+  }
 
-if(2==2){
-  window.addEventListener('scroll', debounce(function(){
-      menuScroll();
-  }, 10));
-}
+})
+
+
